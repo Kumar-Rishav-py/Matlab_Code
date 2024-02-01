68c4941c6b95da0c357530ed10b245d667ea20c5
Kumar Rishav <81737582+Kumar-Rishav-py@users.noreply.github.com>
2024-02-01 18:41:57 +0530

    Gaussian_Plot.m

---
+++ Gaussian_Plot.m
@@ -0,0 +1,32 @@
+clc
+clear all
+close all
+
+x = rand(1, 5000) - 0.5;
+x = sort(x);
+sigma = 0.5;
+mean_val = 0;
+
+y = (1 / (sqrt(2 * pi * sigma^2))) * exp(-(mean_val - x).^2 / (2 * sigma^2));
+
+plot(x, y);
+
+hold on;
+
+sigma1 = 1;
+y1 = (1 / (sqrt(2 * pi * sigma1^2))) * exp(-(mean_val - x).^2 / (2 * sigma1^2));
+plot(x, y1);
+
+hold on;
+sigma2 = 1.1;
+y2 = (1 / (sqrt(2 * pi * sigma2^2))) * exp(-(mean_val - x).^2 / (2 * sigma2^2));
+plot(x, y2);
+
+legend('y', 'y1', 'y2');
+
+hold on;
+mean_val1 = -0.5;
+
+y3 = (1 / (sqrt(2 * pi * sigma^2))) * exp(-(mean_val1 - x).^2 / (2 * sigma^2));
+xlim([-3, 3]);  
+plot(x, y3);
